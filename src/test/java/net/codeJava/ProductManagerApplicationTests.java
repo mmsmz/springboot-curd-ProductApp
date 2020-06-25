bcbbee59678d9692ac6d6db2c41c60383bd1a9fe
Mohamed Arafath <35114431+mmsmz@users.noreply.github.com>
2020-06-25 18:49:46 +0530

    creating a spring boot ProductApp with CRUD operations

---
+++ src/test/java/net/codeJava/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
